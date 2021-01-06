37557b939b7f66dd21adf04df6ac4a3e7503f0b2
iriSVakjyrie <seiryuu2772@gmail.com>
2021-01-07 00:35:38 +0900

    曲選択画面の素材追加　inchの譜面とジャケットを追加　曲の基本構造の追加

--- Assets/Scripts/Music.cs
+++ Assets/Scripts/Music.cs
@@ -6,11 +6,58 @@ public class Music
 {
     public string Name;
 
-    public AudioClip z;
+    public AudioClip MusicData;
+
+    public TextAsset NotesFile_easy;
+    public TextAsset NotesFile_normal;
+    public TextAsset NotesFile_hard;
+    public TextAsset NotesFile_expart;
 
 
+    public int BPM;
+
+    public Dif Difficulty;
+
+    public int Level;
+
+    public Texture2D Jacket;
 
 
+    public enum Dif
+    {
+
+        easy,
+        normal,
+        hard,
+        expart,
+
+
+    }
+
+
+    public Music(string name, int bpm, int level)
+    {
+        Name = name;
+
+
+        MusicData = Resources.Load<AudioClip>(Name);
+
+        NotesFile_easy = Resources.Load<TextAsset>(Name+ "_es");
+        NotesFile_normal = Resources.Load<TextAsset>(Name + "_nor");
+        NotesFile_hard = Resources.Load<TextAsset>(Name + "_har");
+        NotesFile_expart = Resources.Load<TextAsset>(Name + "_ex");
+
+        BPM = bpm;
+
+        
+
+        Level = level;
+
+        Jacket = Resources.Load<Texture2D>(Name);
+
+    }
+
+   
 
 
 
--- Assets/Scripts/MusicDataBase.cs
+++ Assets/Scripts/MusicDataBase.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class MusicDataBase : MonoBehaviour
 {
+    public List<Music> musics = new List<Music>();
+
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        musics.Add(new Music("inch", 191, 12));
+
+
+
     }
 
     // Update is called once per frame
