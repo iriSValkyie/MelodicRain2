4c6a250e45a423a1678ce5fb2a861238d236349f
iriSVakjyrie <seiryuu2772@gmail.com>
2023-09-24 04:31:55 +0900

    プロジェクト整理,ライブラリ追加

---
+++ Assets/OldMelodicRain/Scripts/LoadingNextScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// シーンをロードするスクリプト
+/// </summary>
+/// 
+public class LoadingNextScene : MonoBehaviour
+{
+    AsyncOperation async;//非同期動作
+    [SerializeField] GameObject LoadCanvas;//背景画面
+    [SerializeField] Slider slider;//ロードゲージ
+    // Start is called before the first frame update
+   
+     public void NextScene()
+    {
+        LoadCanvas.SetActive(true);
+        StartCoroutine("LoadScene");
+    }
+    IEnumerator LoadScene()//ロードする
+    {
+        async = SceneManager.LoadSceneAsync("2Dmusic");
+        
+        while (!async.isDone)
+        {
+            var progressVal = Mathf.Clamp01(async.progress / 0.9f);
+            slider.value = progressVal;
+            yield return null;
+        }
+       
+    }
+}
