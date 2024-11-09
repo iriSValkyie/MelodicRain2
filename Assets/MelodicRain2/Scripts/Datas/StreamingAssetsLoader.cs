92a502bcae8a9bf6bc1435ceaeb546c70b4ac975
iriSValkyie <seiryuu2772@gmail.com>
2024-11-10 02:47:21 +0900

    add:StreamingAssets用のアセット見込み処理と共通化のためのインターフェースを追加(#5)

---
+++ Assets/MelodicRain2/Scripts/Datas/StreamingAssetsLoader.cs
@@ -0,0 +1,34 @@
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MelodicRain2
+{
+    /// <summary>
+    /// StreamingAssetsに対してのアセットローダー
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class StreamingAssetsLoader<T>:IAssetLoadable<T>
+    {
+        private UnityWebRequest unityWebRequest;
+        
+        public async UniTask<T> Load(string path)
+        {
+            if (!System.IO.File.Exists(path.Replace("file://", "")))
+            {
+                Debug.LogError($"StreamingAssetsLoader:File {path} does not exist");
+                return default;
+            }
+            unityWebRequest = UnityWebRequest.Get(path);
+            await unityWebRequest.SendWebRequest();
+            return Convert(unityWebRequest);
+        }
+
+        /// <summary>
+        /// ジェネリック型に変換するための処理
+        /// </summary>
+        /// <param name="unityWebRequest"></param>
+        /// <returns></returns>
+        protected abstract T Convert(UnityWebRequest unityWebRequest);
+    }
+}
